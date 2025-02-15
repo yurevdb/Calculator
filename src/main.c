784a5663c1b8d87dcf1cd9f6b60801de4ad54b6f
Yurevdb <vanderbruggen.yure@gmail.com>
2025-02-15 21:35:09 +0100

    bla

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include "stdio.h"
+#include "stdlib.h"
 
-int main() {
-  printf("Hello World\n");
+void print_prompt() { printf("calc > "); }
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    printf("Computing -> %s\n", argv[1]);
+  } else {
+    while (1) {
+      print_prompt();
+      // TODO: read input
+      exit(EXIT_SUCCESS);
+    }
+  }
 }
